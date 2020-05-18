059109ff8ae8c4d95a4a7d75fb93731563efee72
yhoo2160 <37244479+yhoo2160@users.noreply.github.com>
2020-05-18 14:19:40 +0800

    針對Google Search的改動調整過濾的詞彙

--- SEO查詢爬蟲.py
+++ SEO查詢爬蟲.py
@@ -1,22 +1,18 @@
-#SEO爬蟲
-#python3
-#需要BeautifulSoup4套件
-
+# -*- coding: UTF-8 -*-
 import requests
 from bs4 import BeautifulSoup
 import time
 import math
 
 all_flag = 0
-#下面這行設定要搜尋的關鍵字
+# 下面這行設定要搜尋的關鍵字
 key_word_list=["關鍵字1","關鍵字2","關鍵字3","關鍵字4","關鍵字5"]
-#下面這行設定要匹配的目標網域(部分即可)
+# 下面這行設定要匹配的目標網域(部分即可)
 target_url="你家網頁的網域"
 key_word_cnt = 0
 all_res_list = []
-sep = '&sa=U'
 
-#外迴圈跑關鍵字
+# 外迴圈跑關鍵字
 while all_flag == 0:
     res_list = []
     key_word = key_word_list[key_word_cnt]
@@ -25,33 +21,36 @@ while all_flag == 0:
     page = 0
     i = 0
     
-    #內迴圈跑每個關鍵字的頁面
+    # 內迴圈跑每個搜尋頁面
     while flag_stop == 0 :
         j = i
         page_index = page * 100
         addr = "https://www.google.com.tw/search?q="+key_word+"&num=100&start="+str(page_index)
         res = requests.get(addr)
         soup = BeautifulSoup(res.text, "lxml")
+        all_herf = soup.find_all("a")
 
-        #暫存並檢視搜尋結果
-        for result in soup.find_all("h3", class_="r"):
+        result_all = list()
+        for herf in all_herf:
+            if "<a data-uch=\"1\" href=" and "BNeawe vvjwJb AP7Wnd" in str(herf):
+                result_all.append(herf)
+
+        # 暫存並檢視搜尋結果
+        for result in result_all:
             res_list.append([])
             res_list[i].append(i)
             
             try:
-                res_list[i].append(result.find("a").get_text())
+                res_list[i].append(result.find("div").get_text())
             except AttributeError:
                 res_list[i].append("BLANK OR ERROR")
                 print("text blank i="+str(i))
             
             try:
-                #修改下面這行可以切換要比對網址或是其他欄位
-                tmp_txt = result.find("a").get("href").replace("/url?q=","")
-                tmp_txt2 = tmp_txt.split(sep, 1)[0]
-                res_list[i].append(tmp_txt2)
-                #判斷如果匹配目標網域的話就記錄到結果
-                #並設定停止flag
-                if tmp_txt.find(target_url) != -1:
+                url = result.get("href").replace("/url?q=", "").split("&sa=U", 1)[0]
+                res_list[i].append(url)
+                # 判斷如果匹配目標網域的話就記錄到結果，並設定停止flag
+                if url.find(target_url) != -1:
                     flag_stop = 1
                     cnt = i
             except AttributeError:
@@ -65,9 +64,9 @@ while all_flag == 0:
         else:
             page = page + 1
         
-        #time.sleep(3)
+        time.sleep(10)
 
-    #拼湊輸出文字
+    # 拼湊輸出文字
     if res_list[cnt][2].find(target_url) == -1 :
         rtn_text = ("總共:"+str(i)+"筆搜尋結果，未找到符合"+target_url+"網域的結果。")
         all_res_list.append([])
@@ -82,18 +81,17 @@ while all_flag == 0:
         all_res_list[key_word_cnt].append(rtn_text)
         all_res_list[key_word_cnt].append(res_list[cnt][1])
         all_res_list[key_word_cnt].append(res_list[cnt][2])
-        
+
     
-    
-    #輸出結果
-    #也可以呼叫all_res_list[]取得較詳細的網頁標題與網址
+    # 輸出結果
+    # 也可以呼叫all_res_list[]取得較詳細的網頁標題與網址
     print(key_word+":"+rtn_text)
     
     if key_word_cnt == len(key_word_list) - 1:
         all_flag = 1
     else:
         key_word_cnt = key_word_cnt + 1
-        #time.sleep(30)
+        time.sleep(30)
 
 
 
